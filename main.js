7092e4e80e1dab779ba1e48d5ade722cb3472664
Erremme <Erremme2.0@gmail.com>
2025-04-29 18:43:35 +0200

    snack- 2

--- main.js
+++ main.js
@@ -14,4 +14,46 @@
 
  getPostTitle(1)
  .then((obj) => console.log(obj))
- .catch((error) => console.error(error))    
+ .catch((error) => console.error(error)) 
+ 
+
+ 
+ function getPost(id){
+    return new Promise((resolve, reject)=> {
+        fetch(`https://dummyjson.com/posts/${id}`)
+        .then((response) => response.json())
+        .then((obj) => resolve(obj))
+        .then((response) => response.json())    
+        .catch(reject)
+
+        
+    })
+ }
+
+ getPost(1)
+    .then((obj) => console.log(obj))
+    .catch((error) => console.error(error))
+
+    /*
+    Crea la funzione lanciaDado() che restituisce una Promise che, dopo 3 secondi, genera un numero casuale tra 1 e 6. Tuttavia, nel 20% dei casi, il dado si "incastra" e la Promise va in reject.
+    */
+
+function lanciaDado() {
+    return new Promise((resolve, reject) => {
+        console.log("Lancio il dado...")
+        setTimeout(() => {
+            const randomNumber = Math.floor(Math.random() * 6) + 1;
+            const isStuck = Math.random() < 0.2; // 
+            if (isStuck) {
+                reject("Il dado si è incastrato!");
+            } else {
+                resolve(randomNumber);
+            }
+
+        }, 3000)
+    })
+}
+
+lanciaDado()
+    .then((result) => console.log(result))
+    .catch((error) => console.error(error))
